0244a03edb58e070a57a82bd856ed56c9ea23c12
gustavborestam <gustav.borestam@hotmail.com>
2021-09-08 16:54:48 +0200

    fixed code style

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -1,28 +1,31 @@
-import math
-from .. utils import TranspileTestCase, BuiltinFunctionTestCase
+from .. utils import TranspileTestCase
+
 
 class MathTests(TranspileTestCase):
-	def test_ceil(self):
-		self.assertCodeExecution("""
-			import math
-			x = 1.78
-			print(math.ceil(x))
-		""")
-	def test_ceil_neg_float(self):
-		self.assertCodeExecution("""
-			import math
-			x = -2.78
-			print(math.ceil(x))
-		""")
-	def test_ceil_int(self):
-		self.assertCodeExecution("""
-			import math
-			x = 34
-			print(math.ceil(x))
-		""")
-	def test_ceil_neg_int(self):
-		self.assertCodeExecution("""
-			import math
-			x = -2
-			print(math.ceil(x))
-		""")	
+    def test_ceil(self):
+        self.assertCodeExecution("""
+            import math
+            x = 1.78
+            print(math.ceil(x))
+        """)
+
+    def test_ceil_neg_float(self):
+        self.assertCodeExecution("""
+            import math
+            x = -2.78
+            print(math.ceil(x))
+        """)
+
+    def test_ceil_int(self):
+        self.assertCodeExecution("""
+            import math
+            x = 34
+            print(math.ceil(x))
+        """)
+
+    def test_ceil_neg_int(self):
+        self.assertCodeExecution("""
+            import math
+            x = -2
+            print(math.ceil(x))
+        """)
