240628e34f0bc3432560377e8e49d2ae34677baa
gustavborestam <gustav.borestam@hotmail.com>
2021-09-08 16:32:20 +0200

    improved test names

--- tests/stdlib/test_math.py
+++ tests/stdlib/test_math.py
@@ -2,27 +2,27 @@ import math
 from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 class MathTests(TranspileTestCase):
-	def test_float_ceil(self):
+	def test_ceil(self):
 		self.assertCodeExecution("""
 			import math
 			x = 1.78
 			print(math.ceil(x))
 		""")
-	def test_neg_float_ceil(self):
+	def test_ceil_neg_float(self):
 		self.assertCodeExecution("""
 			import math
 			x = -2.78
 			print(math.ceil(x))
 		""")
-	def test_int_ceil(self):
+	def test_ceil_int(self):
 		self.assertCodeExecution("""
 			import math
 			x = 34
 			print(math.ceil(x))
 		""")
-	def test_neg_int_ceil(self):
+	def test_ceil_neg_int(self):
 		self.assertCodeExecution("""
 			import math
 			x = -2
 			print(math.ceil(x))
-		""")
+		""")	
