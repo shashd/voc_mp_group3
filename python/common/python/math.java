a76130386a5b96d02fbe82fc77f472f6686db5d7
Anton <noten.b@hotmail.com>
2021-09-09 10:14:24 +0200

    📖 Added comments and fixed case

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -20,17 +20,18 @@ public class math extends org.python.types.Module {
 
 
     @org.python.Method(
-        __doc__ = "Return the absolute value of x.",
+        __doc__ = "Return the absolute value of x as a float.",
         args = {"x"}
     )
     public static org.python.Object fabs(org.python.Object number) {    
         if (number instanceof org.python.types.Float) {
-            org.python.types.Float castedNumber = (org.python.types.Float)number.__float__();
-            return new org.python.types.Float(Math.abs(castedNumber.value));
+            org.python.types.Float casted_number = (org.python.types.Float)number.__float__();
+            return new org.python.types.Float(Math.abs(casted_number.value));
         }
         if (number instanceof org.python.types.Int) {
-            org.python.types.Int castedNumber = (org.python.types.Int)number.__int__();
-            return new org.python.types.Float(Math.abs(castedNumber.value));
+            org.python.types.Int casted_number = (org.python.types.Int)number.__int__();
+            // Fabs always returns a float, even if the input is an int
+            return new org.python.types.Float(Math.abs(casted_number.value));
         }
 
         throw new org.python.exceptions.TypeError("must be real number, not " + number.typeName() + "");
