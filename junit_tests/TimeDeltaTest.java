4a95145f9bd936d970a4830b14ba9e844ad09438
Anton <noten.b@hotmail.com>
2021-09-16 17:26:50 +0200

    📆 Timedelta init

---
+++ junit_tests/TimeDeltaTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import org.python.Object;
+import org.python.stdlib.datetime.TimeDelta;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimeDeltaTest {
+
+    @Test
+    public void testConstructor() {
+        {
+            HashMap<String, Object> kwargs = new HashMap<>();
+
+            kwargs.put("days", org.python.types.Int.getInt(1));
+            TimeDelta timeDelta = new TimeDelta(new Object[0], kwargs);
+
+            assertEquals(org.python.types.Int.getInt(1), timeDelta.days);
+        }
+
+        {
+            HashMap<String, Object> kwargs = new HashMap<>();
+
+            kwargs.put("weeks", org.python.types.Int.getInt(1));
+            kwargs.put("days", org.python.types.Int.getInt(2));
+            TimeDelta timeDelta = new TimeDelta(new Object[0], kwargs);
+
+            assertEquals(org.python.types.Int.getInt(9), timeDelta.days);
+        }
+    }
+
+}
